6822585b932a94f14186fbfcac5d419842e74b29
ingmar <ingmar@liibert.ee>
2018-09-10 17:35:03 +0300

    Grabber stuff

--- pibot_gazebo/ir_plugin.cpp
+++ pibot_gazebo/ir_plugin.cpp
@@ -9,68 +9,75 @@
 #include <std_msgs/Float64MultiArray.h>
 #include <string>
 
-namespace gazebo {
-  class IRPlugin : public SensorPlugin {
-  private:
-    std::unique_ptr<ros::NodeHandle> rosNode;
-    sensors::RaySensorPtr raySensor;
-    ros::Publisher rawPublisher;
-    ros::Publisher publisher;
-    std::string topic;
-    event::ConnectionPtr updateConnection;
+namespace gazebo
+{
+class IRPlugin : public SensorPlugin
+{
+private:
+  std::unique_ptr<ros::NodeHandle> rosNode;
+  sensors::RaySensorPtr raySensor;
+  ros::Publisher rawPublisher;
+  ros::Publisher publisher;
+  std::string topic;
+  event::ConnectionPtr updateConnection;
 
-  public:
-    void Load(sensors::SensorPtr sensor, sdf::ElementPtr sdf) {
-      if (!ros::isInitialized()) {
-        ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized");
-        return;
-      }
-
-      rosNode.reset(new ros::NodeHandle("gazebo_client"));
-      raySensor = std::dynamic_pointer_cast<sensors::RaySensor>(sensor);
-      topic = sdf->GetElement("topic")->GetValue()->GetAsString();
-
-      rawPublisher = rosNode->advertise<std_msgs::Float64MultiArray>("/robot/"+topic+"/raw", 1);
-      publisher = rosNode->advertise<std_msgs::Float64>("/robot/"+topic+"/value", 1);
-
-      updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&IRPlugin::OnUpdate, this));
+public:
+  void Load(sensors::SensorPtr sensor, sdf::ElementPtr sdf)
+  {
+    if (!ros::isInitialized())
+    {
+      ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized");
+      return;
     }
 
-    void fillRawData(std_msgs::Float64MultiArray &msg) {
-      std::vector<double> data;
-      raySensor->Ranges(data);
+    rosNode.reset(new ros::NodeHandle("gazebo_client"));
+    raySensor = std::dynamic_pointer_cast<sensors::RaySensor>(sensor);
+    topic = sdf->GetElement("topic")->GetValue()->GetAsString();
 
-      msg.layout.dim.push_back(std_msgs::MultiArrayDimension());
-      msg.layout.dim[0].size = data.size();
-      msg.layout.dim[0].stride = 1;
-      msg.layout.dim[0].label = "ranges";
+    rawPublisher = rosNode->advertise<std_msgs::Float64MultiArray>("/robot/" + topic + "/raw", 1);
+    publisher = rosNode->advertise<std_msgs::Float64>("/robot/" + topic + "/value", 1);
 
-      msg.data.clear();
-      msg.data.insert(msg.data.end(), data.begin(), data.end());
+    updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&IRPlugin::OnUpdate, this));
+  }
+
+  void fillRawData(std_msgs::Float64MultiArray &msg)
+  {
+    std::vector<double> data;
+    raySensor->Ranges(data);
+
+    msg.layout.dim.push_back(std_msgs::MultiArrayDimension());
+    msg.layout.dim[0].size = data.size();
+    msg.layout.dim[0].stride = 1;
+    msg.layout.dim[0].label = "ranges";
+
+    msg.data.clear();
+    msg.data.insert(msg.data.end(), data.begin(), data.end());
+  }
+
+  void OnUpdate()
+  {
+    raySensor->SetActive(false);
+
+    //Find min
+    double min_range = raySensor->RangeMax();
+    for (int i = 0; i < raySensor->RangeCount(); i++)
+    {
+      double range = raySensor->Range(i);
+      min_range = std::min(range, min_range);
     }
 
-    void OnUpdate() {
-      raySensor->SetActive(false);
+    //Publish raw data
+    std_msgs::Float64MultiArray rawMsg;
+    fillRawData(rawMsg);
+    rawPublisher.publish(rawMsg);
 
-      //Find min
-      double min_range = raySensor->RangeMax();
-      for (int i = 0; i < raySensor->RangeCount(); i++) {
-          double range = raySensor->Range(i);
-          min_range = std::min(range, min_range);
-      }
+    //Publish min value
+    std_msgs::Float64 msg;
+    msg.data = min_range;
+    publisher.publish(msg);
 
-      //Publish raw data
-      std_msgs::Float64MultiArray rawMsg;
-      fillRawData(rawMsg);
-      rawPublisher.publish(rawMsg);
-
-      //Publish min value
-      std_msgs::Float64 msg;
-      msg.data = min_range;
-      publisher.publish(msg);
-
-      raySensor->SetActive(true);
-    }
-  };
+    raySensor->SetActive(true);
+  }
+};
 GZ_REGISTER_SENSOR_PLUGIN(IRPlugin)
-}
+} // namespace gazebo
--- pibot_gazebo/wheel_plugin.cpp
+++ pibot_gazebo/wheel_plugin.cpp
@@ -15,133 +15,161 @@
 #include <boost/thread.hpp>
 #include <boost/bind.hpp>
 
+#include <math.h>
+
 namespace gazebo
 {
-  class WheelPlugin : public ModelPlugin
+class WheelPlugin : public ModelPlugin
+{
+
+private:
+  physics::JointPtr leftWheelJoint;
+  physics::JointPtr rightWheelJoint;
+  double rightVel;
+  double leftVel;
+
+  // A node use for ROS transport
+  std::unique_ptr<ros::NodeHandle> rosNode;
+
+  // ROS subscribers
+  ros::Subscriber rosSubRightVel;
+  ros::Subscriber rosSubLeftVel;
+
+  //Joint state publisher
+  ros::Publisher rightWheelJointStatePublisher;
+  ros::Publisher leftWheelJointStatePublisher;
+
+  // A ROS callbackqueue that helps process messages
+  ros::CallbackQueue rosQueue;
+
+  // A thread the keeps running the rosQueue
+  std::thread rosQueueThread;
+
+  // Pointer to the model
+  physics::ModelPtr model;
+
+  // Pointer to the update event connection
+  event::ConnectionPtr updateConnection;
+
+  double maxAngularVelocity = 10;
+
+public:
+  void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
   {
+    // Store the pointer to the model
+    this->model = _parent;
 
-    private:
-      physics::JointPtr leftWheelJoint;
-      physics::JointPtr rightWheelJoint;
-      float rightVel;
-      float leftVel;
+    // Initialize ros, if it has not already been initialized.
+    if (!ros::isInitialized())
+    {
+      ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized");
+      return;
+    }
 
-      // A node use for ROS transport
-      std::unique_ptr<ros::NodeHandle> rosNode;
+    this->leftWheelJoint = this->model->GetJoint("left_wheel_hinge");
+    this->rightWheelJoint = this->model->GetJoint("right_wheel_hinge");
 
-      // ROS subscribers
-      ros::Subscriber rosSubRightVel;
-      ros::Subscriber rosSubLeftVel;
+    this->rightVel = 0.0;
+    this->leftVel = 0.0;
 
-      //Joint state publisher
-      ros::Publisher rightWheelJointStatePublisher;
-      ros::Publisher leftWheelJointStatePublisher;
+    // Create our ROS node.
+    this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
 
-      // A ROS callbackqueue that helps process messages
-      ros::CallbackQueue rosQueue;
+    // Create named topics, and subscribe to them.
+    ros::SubscribeOptions soRightVel =
+        ros::SubscribeOptions::create<std_msgs::Float32>(
+            "/robot/wheel/right/vel_cmd",
+            1,
+            boost::bind(&WheelPlugin::OnRightVelCmd, this, _1),
+            ros::VoidPtr(), &this->rosQueue);
 
-      // A thread the keeps running the rosQueue
-      std::thread rosQueueThread;
+    ros::SubscribeOptions soLeftVel =
+        ros::SubscribeOptions::create<std_msgs::Float32>(
+            "/robot/wheel/left/vel_cmd",
+            1,
+            boost::bind(&WheelPlugin::OnLeftVelCmd, this, _1),
+            ros::VoidPtr(), &this->rosQueue);
 
-      // Pointer to the model
-      physics::ModelPtr model;
+    this->rosSubRightVel = this->rosNode->subscribe(soRightVel);
+    this->rosSubLeftVel = this->rosNode->subscribe(soLeftVel);
 
-      // Pointer to the update event connection
-      event::ConnectionPtr updateConnection;
+    //Create joint state publishers
+    this->rightWheelJointStatePublisher = this->rosNode->advertise<std_msgs::Int32>("/robot/wheel/right/position", 1000);
+    this->leftWheelJointStatePublisher = this->rosNode->advertise<std_msgs::Int32>("/robot/wheel/left/position", 1000);
 
-      double maxAngularVelocity = 136;
+    // Spin up the queue helper thread.
+    this->rosQueueThread =
+        std::thread(std::bind(&WheelPlugin::QueueThread, this));
 
-    public:
-      void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/) {
-        // Store the pointer to the model
-        this->model = _parent;
+    // Listen to the update event. This event is broadcast every simulation iteration.
+    this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+        std::bind(&WheelPlugin::OnUpdate, this));
+  }
 
-        // Initialize ros, if it has not already been initialized.
-        if (!ros::isInitialized()) {
-          ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized");
-          return;
-        }
+  void OnLeftVelCmd(const std_msgs::Float32ConstPtr &msg)
+  {
+    //ROS_INFO_STREAM("msg->data is" << msg->data);
+    this->leftVel = this->getVelocity(msg->data);
+    //ROS_INFO_STREAM("leftvel is " << this->leftVel);
+  }
 
-        this->leftWheelJoint = this->model->GetJoint("left_wheel_hinge");
-        this->rightWheelJoint = this->model->GetJoint("right_wheel_hinge");
+  void OnRightVelCmd(const std_msgs::Float32ConstPtr &msg)
+  {
+    //ROS_INFO_STREAM("msg->data is" << msg->data);
+    this->rightVel = this->getVelocity(msg->data);
+    //ROS_INFO_STREAM("rightvel is " << this->rightVel);
+  }
 
-        this->rightVel = 0.0;
-        this->leftVel = 0.0;
+  void publishJointStates()
+  {
+    std_msgs::Int32 rightWheelPosition;
+    std_msgs::Int32 leftWheelPosition;
 
-        // Create our ROS node.
-        this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
+    float radInDeg = 180 / 3.14;
 
-        // Create named topics, and subscribe to them.
-        ros::SubscribeOptions soRightVel =
-          ros::SubscribeOptions::create<std_msgs::Float32>(
-              "/robot/wheel/right/vel_cmd",
-              1,
-              boost::bind(&WheelPlugin::OnRightVelCmd, this, _1),
-              ros::VoidPtr(), &this->rosQueue);
+    rightWheelPosition.data = (int)(this->rightWheelJoint->Position() * radInDeg);
+    leftWheelPosition.data = (int)(this->leftWheelJoint->Position() * radInDeg);
 
-        ros::SubscribeOptions soLeftVel =
-          ros::SubscribeOptions::create<std_msgs::Float32>(
-              "/robot/wheel/left/vel_cmd",
-              1,
-              boost::bind(&WheelPlugin::OnLeftVelCmd, this, _1),
-              ros::VoidPtr(), &this->rosQueue);
+    this->rightWheelJointStatePublisher.publish(rightWheelPosition);
+    this->leftWheelJointStatePublisher.publish(leftWheelPosition);
+  }
 
-        this->rosSubRightVel = this->rosNode->subscribe(soRightVel);
-        this->rosSubLeftVel = this->rosNode->subscribe(soLeftVel);
+  template <typename T>
+  int getSign(T x)
+  {
+    return (x > 0) - (x < 0);
+  }
 
-        //Create joint state publishers
-        this->rightWheelJointStatePublisher = this->rosNode->advertise<std_msgs::Int32>("/robot/wheel/right/position", 1000);
-        this->leftWheelJointStatePublisher = this->rosNode->advertise<std_msgs::Int32>("/robot/wheel/left/position", 1000);
+  double getVelocity(float percentage)
+  {
+    float x = abs(percentage);
+    if (x < 15)
+      return 0.0;
 
-        // Spin up the queue helper thread.
-        this->rosQueueThread =
-          std::thread(std::bind(&WheelPlugin::QueueThread, this));
+    double y = 143.7422 + (-97.04175 - 143.7422) / (1 + pow((x / 20.43845), 0.9319634));
 
-        // Listen to the update event. This event is broadcast every simulation iteration.
-        this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-            std::bind(&WheelPlugin::OnUpdate, this));
-      }
+    return getSign(percentage) * y;
+  }
 
-      void OnLeftVelCmd(const std_msgs::Float32ConstPtr &msg) {
-        this->leftVel = ((msg->data)/100.0)*this->maxAngularVelocity;
-      }
+  // Called by the world update start event
+  void OnUpdate()
+  {
+    // Apply velocity to wheel joints
+    this->leftWheelJoint->SetVelocity(0, this->leftVel);
+    this->rightWheelJoint->SetVelocity(0, this->rightVel);
+    publishJointStates();
+  }
 
-      void OnRightVelCmd(const std_msgs::Float32ConstPtr &msg) {
-        this->rightVel = ((msg->data)/100.0)*this->maxAngularVelocity;
-      }
-
-      void publishJointStates() {
-        std_msgs::Int32 rightWheelPosition;
-        std_msgs::Int32 leftWheelPosition;
-
-        float radInDeg = 180 / 3.14;
-
-        rightWheelPosition.data = (int) (this->rightWheelJoint->Position() * radInDeg);
-        leftWheelPosition.data = (int) (this->leftWheelJoint->Position() * radInDeg);
-
-        this->rightWheelJointStatePublisher.publish(rightWheelPosition);
-        this->leftWheelJointStatePublisher.publish(leftWheelPosition);
-      }
-
-      // Called by the world update start event
-      void OnUpdate() {
-        // Apply velocity to wheel joints
-        this->leftWheelJoint->SetVelocity(0, this->leftVel);
-        this->rightWheelJoint->SetVelocity(0, this->rightVel);
-        publishJointStates();
-	ROS_INFO_STREAM("left joint" << this->leftWheelJoint->GetVelocity(0));
-        ROS_INFO_STREAM("right joint" << this->rightWheelJoint->GetVelocity(0));
-      }
-
-    private:
-      void QueueThread() {
-        static const double timeout = 0.01;
-        while (this->rosNode->ok())
-        {
-          this->rosQueue.callAvailable(ros::WallDuration(timeout));
-        }
-      }
-  };
-  // Register this plugin with the simulator
-  GZ_REGISTER_MODEL_PLUGIN(WheelPlugin)
-}
+private:
+  void QueueThread()
+  {
+    static const double timeout = 0.01;
+    while (this->rosNode->ok())
+    {
+      this->rosQueue.callAvailable(ros::WallDuration(timeout));
+    }
+  }
+};
+// Register this plugin with the simulator
+GZ_REGISTER_MODEL_PLUGIN(WheelPlugin)
+} // namespace gazebo
