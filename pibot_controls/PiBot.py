e001a423b2d7e252e43446f56b22dbd386be5e51
ingmarliibert <ingmar.liibert@gmail.com>
2018-10-11 09:26:13 +0300

    is simulation

--- pibot_controls/PiBot.py
+++ pibot_controls/PiBot.py
@@ -21,7 +21,7 @@ def validate_speed_percentage(speed_function):
 
 
 class PiBot:
-    def is_simulation():
+    def is_simulation(self):
         return True
     
     def make_callback_for_sensor(self, attribute_name):
