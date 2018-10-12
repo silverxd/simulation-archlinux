7560443189dcadef600586c32dd589711200286a
ingmar <ingmar@liibert.ee>
2018-10-12 17:23:59 +0300

    update robots script

---
+++ pibot_controls/update_robots.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo $#
+count=$#
+for ((i=0; i<$count; i++)){
+    if [ $1 -eq 1 ]; then
+        DIR='robot'
+    else
+        DIR='pibot'
+    fi
+    sshpass -p 'pibot' ssh pi@192.168.0.9$1 "cd $DIR && git pull origin master"
+    echo "Updated $1"
+    shift
+}
+echo "Thank you, come again!"
