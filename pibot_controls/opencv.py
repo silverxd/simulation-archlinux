d442952140e747678761c4c58003bf17d5dde235
Gert Kanter <gert.kanter@ttu.ee>
2020-11-10 22:29:43 +0200

    Add multi-object detection

--- pibot_controls/opencv.py
+++ pibot_controls/opencv.py
@@ -27,7 +27,7 @@ class ImageProcessor:
         if src is None:
             return []
         cv2.imwrite("/home/iti0201/opencv_orig.png", src)
-        hsv = cv2.cvtColor(src, cv2.COLOR_RGB2HSV)
+        hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
         mask_red = self.get_mask(hsv, 0, 20)
         mask_blue = self.get_mask(hsv, 240/2, 20)
         res = cv2.bitwise_and(src,src, mask= mask_red)
@@ -35,22 +35,22 @@ class ImageProcessor:
         dst = cv2.add(res, res2)
         return_value = []
         contours, _ = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        return_value.append(len(contours))
-        if len(contours) > 0 and len(contours[0]) > 5:
-            ((x, y), radius) = cv2.minEnclosingCircle(contours[0])
+        for contour in contours:
+           if len(contour) > 5:
+            ((x, y), radius) = cv2.minEnclosingCircle(contour)
             cv2.circle(src, (int(x), int(y)), int(radius), (0,255,0), 2)
             cv2.circle(src, (int(x), int(y)), 1, (255, 255, 255), 2)
-            return_value.append((int(x), int(y)))
+            return_value.append(("red sphere", (int(x), int(y)), int(radius)))
         contours, _ = cv2.findContours(mask_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        return_value.append(len(contours))
-        if len(contours) > 0 and len(contours[0]) > 5:
-            ((x, y), radius) = cv2.minEnclosingCircle(contours[0])
+        for contour in contours:
+           if len(contour) > 5:
+            ((x, y), radius) = cv2.minEnclosingCircle(contour)
             cv2.circle(src, (int(x), int(y)), int(radius), (0,255,0), 2)
             #ellipse = cv2.fitEllipse(contours[0])
             #cv2.ellipse(src,ellipse,(0,255,0),2)
             #center = (int(ellipse[0][0]), int(ellipse[0][1]))
             cv2.circle(src, (int(x), int(y)), 1, (255, 255, 255), 2)
-            return_value.append((int(x), int(y)))
+            return_value.append(("blue sphere", (int(x), int(y)), int(radius)))
         cv2.imwrite("/home/iti0201/opencv.png", src)
         return return_value
 
