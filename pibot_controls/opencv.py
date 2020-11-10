e86ae50c3fdb31fce172900b0e84ea8689dcec37
Gert Kanter <gert.kanter@ttu.ee>
2020-11-10 21:50:04 +0200

    Find single blue and red sphere with OpenCV

--- pibot_controls/opencv.py
+++ pibot_controls/opencv.py
@@ -19,40 +19,39 @@ class ImageProcessor:
 
     def convert_sensor_msgs_image_to_opencv_image(self, sensor_image):
         if self.height is not None and self.width is not None:
-            mat = np.zeros((self.height, self.width, 3), np.uint8)
-            for i in range(self.height):
-                for j in range(0, self.width * 3, 3):
-                    for k in range(3):
-                        mat[i, j//3, k] = sensor_image[i * self.width + j + k]
-            return mat
+            return np.ndarray((self.height, self.width, 3), np.uint8, buffer=sensor_image)
         return None
 
     def get_objects(self, image):
         src = self.convert_sensor_msgs_image_to_opencv_image(image)
         if src is None:
             return []
-        
+        cv2.imwrite("/home/iti0201/opencv_orig.png", src)
         hsv = cv2.cvtColor(src, cv2.COLOR_RGB2HSV)
         mask_red = self.get_mask(hsv, 0, 20)
         mask_blue = self.get_mask(hsv, 240/2, 20)
         res = cv2.bitwise_and(src,src, mask= mask_red)
         res2 = cv2.bitwise_and(src,src, mask= mask_blue)
         dst = cv2.add(res, res2)
-        contours, _ = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         return_value = []
+        contours, _ = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        return_value.append(len(contours))
         if len(contours) > 0 and len(contours[0]) > 5:
-            ellipse = cv2.fitEllipse(contours[0])
-            cv2.ellipse(src,ellipse,(0,255,0),2)
-            center = (int(ellipse[0][0]), int(ellipse[0][1]))
-            cv2.circle(src, center, 1, (255, 255, 255), 2)
-            return_value.append(center)
+            ((x, y), radius) = cv2.minEnclosingCircle(contours[0])
+            cv2.circle(src, (int(x), int(y)), int(radius), (0,255,0), 2)
+            cv2.circle(src, (int(x), int(y)), 1, (255, 255, 255), 2)
+            return_value.append((int(x), int(y)))
         contours, _ = cv2.findContours(mask_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        return_value.append(len(contours))
         if len(contours) > 0 and len(contours[0]) > 5:
-            ellipse = cv2.fitEllipse(contours[0])
-            cv2.ellipse(src,ellipse,(0,255,0),2)
-            center = (int(ellipse[0][0]), int(ellipse[0][1]))
-            cv2.circle(src, center, 1, (255, 255, 255), 2)
-            return_value.append(center)
+            ((x, y), radius) = cv2.minEnclosingCircle(contours[0])
+            cv2.circle(src, (int(x), int(y)), int(radius), (0,255,0), 2)
+            #ellipse = cv2.fitEllipse(contours[0])
+            #cv2.ellipse(src,ellipse,(0,255,0),2)
+            #center = (int(ellipse[0][0]), int(ellipse[0][1]))
+            cv2.circle(src, (int(x), int(y)), 1, (255, 255, 255), 2)
+            return_value.append((int(x), int(y)))
+        cv2.imwrite("/home/iti0201/opencv.png", src)
         return return_value
 
 if __name__ == "__main__":
