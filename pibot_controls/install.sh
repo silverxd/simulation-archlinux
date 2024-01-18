b25e157a1f57f940e3ab73f1fc90ffcff7bf705c
Gert Kanter <gert.kanter@taltech.ee>
2024-01-18 05:04:26 +0000

    Update for Docker

--- pibot_controls/install.sh
+++ pibot_controls/install.sh
@@ -9,28 +9,7 @@ CYAN_B='\033[1;36m'
 YELLOW='\033[1;33m'
 NC='\033[0m' #  No Color
 
-if ! [ ${#ROS_DISTRO} -ge 1 ]; then
-  #  Install ROS
-  printf "${RED}ROS IS NOT DETECTED!!! Initializing automatic install...${NC}\n\n"
-  sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-  sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-  sudo apt update
-  sudo apt-get install -y ros-noetic-desktop-full
-  echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-  source ~/.bashrc
-  sudo apt-get install -y python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
-
-  #  Validate ROS installation.
-  if ! [ ${#ROS_DISTRO} -ge 1 ]; then
-    printf "${GREEN}ROS $(rosversion -d) has been successfully installed${NC}\n"
-  else
-    sudo apt-get remove ros-*
-    printf "${RED}Automatic ROS installation failed! Removing all files related to ROS... Please install ROS manually and run this script again!${NC}\n"
-    exit 1
-  fi
-else
-  printf "${GREEN}Successfully detected ROS version... $(rosversion -d)\n\n"
-fi
+source /opt/ros/noetic/setup.bash
 
 #  Setup simulation files.
 printf "${GREEN}Installing ITI0201 simulation...${NC}\n\n"
@@ -52,7 +31,6 @@ printf "${GREEN}Cloning simulation repository...${NC}\n\n"
 git clone https://github.com/iti0201/simulation src
 source ~/.bashrc
 catkin_make -DCMAKE_CXX_FLAGS="-std=c++17"
-sudo rm -rf /usr/bin/robot_test /usr/bin/script_launch
 DIR=$HOME/catkin_ws/src/pibot_controls
 printf "\nCreating symlinks...\n\n"
 sudo rm -rf /usr/bin/robot_test /usr/bin/script_launch /usr/bin/update_files
@@ -78,20 +56,6 @@ if ! [ -x "$(command -v pip3)" ]; then
 else
   printf "${GREEN}pip3 detected!${NC}\n\n"
 fi
-
-#  Check if pip packages are installed.
-printf "Checking packages...\n"
-declare -a packages=("rospkg" "catkin_pkg")
-for package in "${packages[@]}"
-do
-  if python3 -c "import $package" &> /dev/null; then
-    printf "${GREEN}$package detected!${NC}\n"
-  else
-    printf "Installing $package\n"
-    sudo pip3 install  $package
-    printf "${GREEN}$package has been successfully installed!${NC}\n"
-  fi
-done
 
 # Check python directories
 printf "\nChecking python directories...\n"
@@ -130,7 +94,5 @@ To do that write into the terminal ${CYAN}vim ~/.bashrc${NC}
 The source variables at the bottom of the file have to be
 ${CYAN}source /opt/ros/noetic/setup.bash
 source ~/catkin_ws/devel/setup.bash${NC}
-${YELLOW}Now start the simulation${NC}, if it says that ${RED}cannot connect to the roscore${NC} then
-open a new terminal and write ${CYAN_B}roscore${NC} and try the simulation script again.
 ${YELLOW}Now you are done!${NC}\n\n"
 exit 1
